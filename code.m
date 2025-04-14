5d0b569c92c9bf27ac803058e7d55ad09e5ceefa
hanna-abr3 <egyha7@nottingham.ac.uk>
2025-04-14 19:43:03 +0100

    Completed till Q1d, coded the thermistor recording room temps

--- code.m
+++ code.m
@@ -15,13 +15,75 @@ end
 
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+clear
+
+a = arduino()
+analogPin ='A0'
+duration = 600;                    %Duration that data will be collected for 
+
+%preallocate arrays for time, voltage and temperature
+time = zeros(1,duration);         
+voltage = zeros(1,duration);
+temperature = zeros(1,duration);
+
+TC = 0.01;                         %zero degree voltage
+V0 = 0.5;                          %temperature coefficient
+
+%loop to keep collect data from te thermistor every second for the entire
+%duration
+for t = 1:duration
+    volts = readVoltage(a, analogPin);
+    tempC = (volts - V0)/ TC;      %converts voltage into temperature
+    
+    voltage(t) = volts
+    temperature(t) = tempC;
+    time(t) = t
+    pause(1);
+end
+
+%data from the aquired results
+minTemp = min(temperature);
+maxTemp = max(temperature);
+avgTemp = mean(temperature);
 
 
 
+%c)Plot a graph using the results of temperature against time
+plot(time,temperature)
+xlabel('Time,s')
+ylabel('Temperature, °C')
+title('Temperature/Time graph')
+
+
+%d)displays the collected temperature data in the required table
+currentdate = datestr(now, 'dd/mm/yyyy');
+
+%date and location
+fprintf('\n');
+today = datestr(now, 'dd/mm/yyyy');
+fprintf('Data logging initiated - %s\n', today);
+fprintf('Location - London\n\n');
+
+%loop to display the temperature reading at every minute
+for minute = 0:10
+    index = minute*60+1
+    fprintf('Minute\t\t\t%d\n', minute);
+    fprintf('Temperature\t\t%.2f C\n\n', temperature(index));
+end
+
+fprintf('Max temp\t\t%.2f C\n', maxTemp);
+fprintf('Min temp\t\t%.2f C\n', minTemp);
+fprintf('Average temp\t%.2f C\n\n', avgTemp);
+
+% End log
+fprintf('Data logging terminated\n');
+
+
+%e)
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
-% Insert answers here
+
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [25 MARKS]
