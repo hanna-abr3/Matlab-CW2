c83e636d1565f98709804d831dbd99e9a9b4192a
hanna-abr3 <egyha7@nottingham.ac.uk>
2025-05-03 14:07:43 +0100

    finsihed Q2

--- temp_monitor.m
+++ temp_monitor.m
@@ -1,20 +1,45 @@
 clear
 
+function temp_monitor (a, an, g, y, r)
+
 %This is a function which blinks certain LEDs based off the temperature of
 %the room. In the brackets input the variable a, the anaolog pin that the thermistor inputs to, the
-%digitial pin of the green yellow and red LED's in that order.
-function y = temp_monitor (a, an, g, y, r)
+%digitial pin of the green, yellow and red LED's in that order. The green
+%LED will trun on steadliy if the temperature is between 18-24C. The yellow
+%LED will blink at s if the temperature is greater than 18C and the red LED
+%will blink at 0.25s if the temperature is less than 24C. A live graph of
+%temperature over time is also plotted.
 
-a = arduino(); 
-analogPin = 'an'; 
-greenLED = 'g';
-yellowLED = 'y';
-redLED = 'r';
+% a = arduino object
+% an = analogue pin for the thermistor
+% g = digital pin for the green LED
+% y = digital pin for the yellow LED
+% r = digital pin for the red LED
 
 TC = 0.01;  % Temperature coefficient
 V0 = 0.5;   % 0 degree voltage
 
+figure
     while true
+
+         elapsed = toc(startTime);
+        volts = readVoltage(a, an);
+        tempC = (volts - V0) / TC;
+
+        % Store data
+        t(end+1) = elapsed;
+        tempLog(end+1) = tempC;
+
+        % Plot live graph
+        plot(t, tempLog, 'b', 'LineWidth', 2);
+        xlabel('Time (s)');
+        ylabel('Temperature (°C)');
+        title('Live Temperature Monitoring');
+        grid on;
+        xlim([max(0, elapsed - 60), elapsed + 5]);
+        ylim([min(10, min(tempLog) - 1), max(30, max(tempLog) + 1)]);
+        drawnow;
+
       
         volts = readVoltage(a, analogPin);
         tempC = (volts - V0) / TC;
@@ -29,16 +54,16 @@ V0 = 0.5;   % 0 degree voltage
 
   
         if tempC >= 18 & tempC <= 24
-            writeDigitalPin(a, greenLED, 1);
+            writeDigitalPin(a, g, 1);
             pause(0.5); 
         elseif tempC < 18
-            writeDigitalPin(a, yellowLED, 1);
+            writeDigitalPin(a, y, 1);
             pause(0.5);
-            writeDigitalPin(a, yellowLED, 0);
+            writeDigitalPin(a, y, 0);
         else
-            writeDigitalPin(a, redLED, 1);
+            writeDigitalPin(a, r, 1);
             pause(0.25);
-            writeDigitalPin(a, redLED, 0);
+            writeDigitalPin(a, r, 0);
         end
     end
 end
