36ea5b139f3e0a67667b10e04372d51378fb1654
hanna-abr3 <egyha7@nottingham.ac.uk>
2025-05-06 13:45:52 +0100

    Q3 code for the function

--- temp_prediction.m
+++ temp_prediction.m
@@ -1,17 +1,14 @@
 % Hanna Abraham
 % egyha7@nottingham.ac.uk
 
-function temp_prediciton(a)
+function temp_monitor(a, an, g, y, r)
 % a = arduino object
 % an = analogue pin for the thermistor
 % g = digital pin for the green LED
 % y = digital pin for the yellow LED
 % r = digital pin for the red LED
 
-r = 'D3';
-g = 'D4';
-y = 'D2';
-an = 'A0';
+
 
 configurePin(a,r,'DigitalOutput');
 configurePin(a,g,'DigitalOutput');
@@ -22,7 +19,7 @@ configurePin(a,an,'AnalogInput');
 min_comfort_temp = 18
 max_comfort_temp = 24
 Maxchangepermin = 4 %4degreesC per minute 
-rateTheshold = Maxchangepermin / 60
+rateThreshold = Maxchangepermin / 60
 
 %arrays created to store past temp values & time intervals between
 Temp_log = [];
@@ -33,10 +30,10 @@ fprintf ('Monitoring temperature of the room..\n');
 while true
     current_time = datetime ('now');
     voltageReading = readVoltage(a,an);
-    Current_temp = (voltage - 0.5) *100;
+    Current_temp = (voltageReading - 0.5) *100;
 
     Temp_log = [Temp_log, Current_temp];
-    time_log = [time_log, Current_temp];
+    time_log = [time_log, current_time];
 
     if length(Temp_log) >10
         Temp_log = Temp_log(end-9:end);
@@ -45,8 +42,8 @@ while true
 
     if length (Temp_log) > 2
         Temp_diff = Temp_log(end) - Temp_log(1);
-        time_diff = second/time_log(end) - time_log(1);
-        rate = Temp_diff / time_diff;
+        time_diff = seconds(time_log(end) - time_log(1));
+        temp_rate = Temp_diff / time_diff;
     else
         temp_rate = 0
     end
@@ -56,15 +53,15 @@ while true
 
 fprintf('Current Temp: %.2f °C | Rate: %.2f °C/s | Predicted in 5 min: %.2f °C\n', Current_temp, temp_rate, predicted_temp);
 
-    if abs(temp_rate) < rateTheshold
+    if abs(temp_rate) < rateThreshold
         writeDigitalPin(a, g, 1); 
         writeDigitalPin(a, y, 0);
         writeDigitalPin(a, r, 0);
-    elseif rate >= ratechange_temp
+    elseif temp_rate >= rateThreshold
         writeDigitalPin(a, g, 0); 
         writeDigitalPin(a, y, 0);
         writeDigitalPin(a, r, 1);      
-    elseif rate <= ratechange_temp
+    elseif temp_rate <= rateThreshold
         writeDigitalPin(a, g, 0); 
         writeDigitalPin(a, y, 1);
         writeDigitalPin(a, r, 0); 
